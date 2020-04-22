e2a1af1265b9e8fdd69486c32284587412168bef
RelentLessM <keraged0n@gmail.com>
2020-04-22 22:56:33 +0300

    About section added to home page.

---
+++ Web/IGamer.Web/ViewComponents/AboutPolicyViewComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace IGamer.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ViewComponent(Name = "AboutPolicy")]
+    public class AboutPolicyViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return this.View();
+        }
+    }
+}
