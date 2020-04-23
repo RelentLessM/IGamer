703426f092affd40ece502eb3c01ecc34e6e9134
RelentLessM <keraged0n@gmail.com>
2020-04-24 01:49:21 +0300

    Tests for votes add. Test for suggestions almost done.

---
+++ Tests/IGamer.Services.Data.Tests/SuggestionsServiceTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Reflection;
+using IGamer.Services.Data.Suggestions;
+using IGamer.Services.Mapping;
+using IGamer.Web.ViewModels.Suggestions;
+using Moq;
+
+namespace IGamer.Services.Data.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using IGamer.Data;
+    using IGamer.Data.Models;
+    using IGamer.Data.Repositories;
+    using IGamer.Services.Data.Votes;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+
+    public class SuggestionsServiceTest
+    {
+        public SuggestionsServiceTest()
+        {
+            AutoMapperConfig
+                .RegisterMappings(typeof(CreateSuggestionInputModel).GetTypeInfo().Assembly);
+        }
+
+        [Fact]
+        public async Task AddOnSuggestion()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new ApplicationDbContext(options.Options);
+
+            var suggestionGameRepository = new EfDeletableEntityRepository<SuggestionGame>(context);
+            var service = new SuggestionsService(suggestionGameRepository);
+            var model = new CreateSuggestionInputModel()
+            {
+                Description = "SomeDescSomeDescSomeDescSomeDescSomeDescSomeDescSomeDescSomeDescSomeDescSomeDesc",
+                ImageUrl = "google.com",
+                Title = "New SuggestedGame",
+                UserId = "1",
+            };
+            await service.CreateSuggestionAsync(model);
+            var actual = await service.GetAllAsync();
+
+            Assert.Single(actual);
+        }
+
+        [Fact]
+        public async Task GetAllTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new ApplicationDbContext(options.Options);
+
+            var suggestionGameRepository = new EfDeletableEntityRepository<SuggestionGame>(context);
+            var service = new SuggestionsService(suggestionGameRepository);
+            var actual = await service.GetAllAsync();
+
+            Assert.Empty(actual);
+        }
+    }
+}
---
+++ Tests/IGamer.Services.Data.Tests/VotesServiceTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading.Tasks;
+using IGamer.Data;
+using IGamer.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace IGamer.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using IGamer.Data.Common.Repositories;
+    using IGamer.Data.Models;
+    using IGamer.Services.Data.Votes;
+    using Moq;
+    using Xunit;
+
+    public class VotesServiceTest
+    {
+        [Fact]
+        public async Task TwoVotesOnPostFromOneUserShouldCountOnce()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new ApplicationDbContext(options.Options);
+            var postRepository = new EfRepository<VoteOnPost>(context);
+            var guideRepository = new EfRepository<VoteOnGuide>(context);
+            var suggestionGameRepository = new EfRepository<VoteOnSuggestionGame>(context);
+            var service = new VotesService(postRepository, guideRepository, suggestionGameRepository);
+
+            await service.VoteOnPostAsync("1", "1", true);
+            await service.VoteOnPostAsync("1", "1", true);
+            var actual = await service.GetVotesOnPostAsync("1");
+
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public async Task TwoVotesOnGuideFromOneUserShouldCountOnce()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new ApplicationDbContext(options.Options);
+            var postRepository = new EfRepository<VoteOnPost>(context);
+            var guideRepository = new EfRepository<VoteOnGuide>(context);
+            var suggestionGameRepository = new EfRepository<VoteOnSuggestionGame>(context);
+            var service = new VotesService(postRepository, guideRepository, suggestionGameRepository);
+
+            await service.VoteOnGuideAsync("1", "1", true);
+            await service.VoteOnGuideAsync("1", "1", true);
+            var actual = await service.GetVotesOnGuideAsync("1");
+
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public async Task SecondVoteOnSuggestionGameFromOneUserShouldNotCount()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new ApplicationDbContext(options.Options);
+            var postRepository = new EfRepository<VoteOnPost>(context);
+            var guideRepository = new EfRepository<VoteOnGuide>(context);
+            var suggestionGameRepository = new EfRepository<VoteOnSuggestionGame>(context);
+            var service = new VotesService(postRepository, guideRepository, suggestionGameRepository);
+
+            await service.VoteOnSuggestionGameAsync(1, "1");
+            var actual = await service.VoteOnSuggestionGameAsync(1, "1");
+            var expected = "You have already voted for this game!";
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task VoteOnSuggestionGameAdd()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new ApplicationDbContext(options.Options);
+            var postRepository = new EfRepository<VoteOnPost>(context);
+            var guideRepository = new EfRepository<VoteOnGuide>(context);
+            var suggestionGameRepository = new EfRepository<VoteOnSuggestionGame>(context);
+            var service = new VotesService(postRepository, guideRepository, suggestionGameRepository);
+
+            await service.VoteOnSuggestionGameAsync(1, "1");
+            var actual = suggestionGameRepository.All().Count();
+            var expected = 1;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
